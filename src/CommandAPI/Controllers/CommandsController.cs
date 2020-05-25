5f4d413d33021602566bc5009119938586233e31
Carlos Jose Agudelo Parra <carlosjagudelop@gmail.com>
2020-05-24 20:06:10 -0500

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAction(){
+            return new string[] {"this", "is", "hard", "coded"};
+        }
+
+    }
+
+
+}
